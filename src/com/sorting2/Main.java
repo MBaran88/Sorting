5e3016b87650dedc5677be67709cfddd6dc291b2
MBaran88 <zdzibq@gmail.com>
2017-05-19 19:40:35 +0200

    commmit

--- src/com/sorting2/Main.java
+++ src/com/sorting2/Main.java
@@ -3,15 +3,15 @@ package com.sorting2;
 public class Main {
 
     public static void main(String[] args) {
-    int [] a = {2,-4,5,-4,10};
+    int [] a = {2,-4,5,-3,10};
         boolean wasSwap;
         for (int i = 0; i <a.length ; i++) {
             wasSwap = false;
             for (int j = i; j <a.length ; j++) {
                 if (a[i] < a[j]){
                     int tmp = a[j];
-                    a[j] = a[i];
-                    a[i] = tmp;
+                    a[j] = a[j+1];
+                    a[j+1] = tmp;
                     wasSwap = true;
                 }
             }
