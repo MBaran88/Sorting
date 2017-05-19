9ef49546a88c41c78ba723072821ae465687a009
MBaran88 <zdzibq@gmail.com>
2017-05-19 19:25:42 +0200

    commmit

--- src/com/sorting2/Main.java
+++ src/com/sorting2/Main.java
@@ -4,14 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
     int [] a = {2,-4,5,-4,10};
-
+        boolean wasSwap;
         for (int i = 0; i <a.length ; i++) {
+            wasSwap = false;
             for (int j = i; j <a.length ; j++) {
                 if (a[i] < a[j]){
                     int tmp = a[j];
                     a[j] = a[i];
                     a[i] = tmp;
+                    wasSwap = true;
                 }
+            }
+            if(!wasSwap) {
+                break;
             }
         }
         for (int i = 0; i <a.length ; i++) {
